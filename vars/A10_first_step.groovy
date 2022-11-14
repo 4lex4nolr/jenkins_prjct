7c7054c1911af1302de40103e77ecb8d7e60797c
aleks <4lex4nolr@gmail.com>
2022-11-14 22:07:56 +0300

    init commit

--- vars/A10_first_step.groovy
+++ vars/A10_first_step.groovy
@@ -1,6 +1,7 @@
+import org.apache.poi.hssf
 
 def call() {
     script {
-        def wb = new org.apache.poi.xssf.usermodel.XFFSWorkbook()
+        def wb = new org.apache.poi.hssf.usermodel.HSSFWorkbook()
     }
 }
